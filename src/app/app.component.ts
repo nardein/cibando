f09f82d3a655a6abbc8e9519ffa04264fdc7ea90
Leonardo Colonna <leonardocolonnareal@gmail.com>
2025-01-08 14:01:28 +0100

    aggiunto il carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cibando';
+
+  path = "../assets/images/carousel-"
+  images = [
+    {id: 1, label:'Spaghetti al pomodoro'},
+    {id: 2, label:'Tagliata di manzo'},
+    {id: 3, label:'Tiramisù classico'}
+  ];
 }
