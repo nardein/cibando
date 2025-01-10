446eb6eaeb949341af82e741e5faeb80f47d6214
Leonardo Colonna <leonardocolonnareal@gmail.com>
2025-01-10 10:29:26 +0100

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { RecipesComponent } from './components/recipes/recipes.component';
+
+const routes: Routes = [
+  {path:'',redirectTo: 'home',pathMatch: 'full'}, //pathMatch controlla tutta la stringa dell'url e funziona sempre
+  {path: 'home',component: HomeComponent}, //'home' rotta del url e component da caricare
+  {path: 'recipes', component: RecipesComponent},
+  {path: '**', redirectTo: 'home'} // se l'url è sbagliato riporta alla home
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
